61480c1147ea6ee8367b4ff125bec7f355bff9ee
shivam <141029609+js-1608@users.noreply.github.com>
2024-07-30 10:48:38 +0530

    update

---
+++ newfooter.php
@@ -0,0 +1,294 @@
+
+<?php 
+
+
+include('db_config/base_url.php');
+?>
+
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/mega_menu/nfooter.css">
+
+
+<section class="footerforvoila">
+    <div class="footercontainer">
+        <div class="sectiondivision">
+
+            <ul class="footerlinkdivi1">
+                <li class="linklitagfooter"><a class="linkdivi highlight" >Quick Links</a></li>
+                <li class="linklitagfooter"><a class="linkdivi" href="booking_calender.php">Model Calender</a></li>
+                <li class="linklitagfooter"><a class="linkdivi" href="booking_calender_product.php">Product Calender</a></li>
+                <li class="linklitagfooter"><a class="linkdivi" href="profile_current_booking.php">Manage Orders</a></li>
+                <li class="linklitagfooter"><a class="linkdivi" href="voilagram.php" target="_blank">Social</a></li>
+                <!-- <li class="linklitagfooter"><a class="linkdivi" href="sitemap.xml">Studio Live</a></li> -->
+                <li class="linklitagfooter"><a class="linkdivi" href="faq.php">FAQ</a></li>
+            </ul>
+
+            <ul class="footerlinkdivi2">
+                <li class="linklitagfooter"><a class="linkdivi highlight">Policies</a></li>
+                <li class="linklitagfooter"><a class="linkdivi"  href="/product-disptach-policy.php"> Dispatch </a></li>
+                <!-- <li class="linklitagfooter"><a class="linkdivi" href="/">Terms of use</a></li> -->
+                <li class="linklitagfooter"><a class="linkdivi" href="/cancelation.php">Cancellation</a></li>
+                <li class="linklitagfooter"><a class="linkdivi" href="/privacypolicy.php">Privacy </a></li>
+                <li class="linklitagfooter"><a class="linkdivi" href="/refundpolicy.php">Refund</a></li>
+                <li class="linklitagfooter"><a class="linkdivi" href="/Grievance_policy.php">Grievance</a></li>
+
+            </ul>
+          
+
+            <ul class="footerlinkdivi2">
+                <li class="linklitagfooter"><a class="linkdivi highlight">Info</a></li>
+                <li class="linklitagfooter"><a class="linkdivi" href="aboutus.php">About Us</a></li>
+                <!-- <li class="linklitagfooter"><a class="linkdivi" href="pricing.php">Pricing</a></li> -->
+                <!-- <li class="linklitagfooter"><a class="linkdivi" href="model_profile.php">Models</a></li> -->
+                <li class="linklitagfooter"><a class="linkdivi" href="contactus.php">Contact Us</a></li>
+                <li class="linklitagfooter"><a class="linkdivi" href="career.php">Career</a></li>
+                <li class="linklitagfooter"><a class="linkdivi" href="sitemap.xml">Site Map</a></li>
+            </ul>
+
+          
+
+            <ul class="footerlinkdivi2">
+                <li class="linklitagfooter"><a class="linkdivi highlight" href="#">Our Verticals</a></li>
+                <li class="linklitagfooter"><a class="linkdivi" href="https://voilabiz.com/">VOILA Biz</a></li>
+                <li class="linklitagfooter"><a class="linkdivi" href="index.php">VOILA Studio</a></li>
+                <li class="linklitagfooter"><a class="linkdivi" href="https://voilakart.com/">VOILA Kart</a></li>
+                <li class="linklitagfooter"><a class="linkdivi" href="https://voilaretail.com/">VOILA Retail</a></li>
+            </ul>
+
+    <ul class="footerlinkdivi9">
+                <li class="linklitagfooter"><a class="linkdivi" href="#"><img class="imgofvoila" src="<?php echo BASE_URL; ?>/mega_menu/img/voila_studio_logo.svg" alt=""></a></li>
+                <!-- <li class="linklitagfooter"><a class="linkdivi" href="#"> &#169; 2017-<?php echo date("Y"); ?> All Rights Reserved Made & Designed By</a></li>
+                <li class="linklitagfooter"><a class="linkdivi highlight" href="#">Intenim Technologies Pvt Ltd</a></li> -->
+                <li class="linklitagfooter"><a class="linkdivi highlight" href="#">Think Ecommerce Think VOILA</a></li>
+                <span class="socialiconsfootervoila">
+                    <a id="icons_social" href="https://www.facebook.com/VoilaStudio.in/"><img class="socialiconsvoila" src="<?php echo BASE_URL; ?>/mega_menu/img/footer/facebook.webp" alt=""></a>
+
+                    <a id="icons_social" href="https://www.instagram.com/VoilaStudioTM/"><img class="socialiconsvoila" src="<?php echo BASE_URL; ?>/mega_menu/img/footer/instagram.webp" alt=""></a>
+
+                    <a id="icons_social" href="https://in.linkedin.com/showcase/voilastudio"><img class="socialiconsvoila" src="<?php echo BASE_URL; ?>/mega_menu/img/footer/linkedin.webp" alt=""></a>
+
+                    <a id="icons_social" href="https://twitter.com/studiovoila?lang=en"><img class="socialiconsvoila" src="<?php echo BASE_URL; ?>/mega_menu/img/footer/twitter.webp" alt=""></a>
+
+                    <a id="icons_social" href="https://www.youtube.com/channel/UCOYKIJYw3t1dHBK3tnXzjyQ"><img class="socialiconsvoila" src="<?php echo BASE_URL; ?>/mega_menu/img/footer/youtube.webp" alt=""></a>
+
+                    
+                </span>
+            </ul>
+    </div>
+
+
+
+    <div class="sectiondivision2">
+
+        <ul class="footerlinkdivi4">
+            <li class="linklitagfooter"><a class="linkdivi highlight black" href="#">Corporate Office</a></li>
+            <li  class="linklitagfooter"><a style="font-weight: 700;" class="linkdivi" href="#">Intenim Technologies Pvt Ltd.</a></li>
+            <li class="linklitagfooter"><a class="linkdivi" href="#">Unit 1007-1008A, WellDone TechPark,
+            Gurugram, Haryana- 122018, India</a></li>
+            <li class="linklitagfooter"><a class="linkdivi" href="#">Landline : +91 124 4003737</a></li>
+        </ul>
+        <span class="getintouchsppan">
+        <p class="getintouchmesage highlight">Get In Touch !</p>
+        <!-- <ul class="footerlinkdivicenterofline">
+            <li class="linklitagfootercenter"><a class="linkdivi" href="#"></a></li>
+        </ul> -->
+        <hr class="footerlinkdivicenterofline">
+        </span>
+
+        <ul class="footerlinkdivi3getin">
+            <p class="getintouchmesage highlight">Get In Touch !</p>
+            <ul class="footerlinkdivi3">
+                <li class="linklitagfooter numberslinks">Phone :
+                <a style="line-height: 24px;" class="linkdivi" href="tel:+919810554548">+91 9810554548 |</a>
+                <a style="line-height: 24px;" class="linkdivi" href="tel:+919821554548">+91 9821554548 |</a>
+                <a style="line-height: 24px;" class="linkdivi" href="tel:+919821654548">+91 9821654548 </a>
+                <!-- <a style="line-height: 24px;" class="linkdivi" href="tel:+919560628008">+91 9560628008</a> -->
+        </li>
+                <li class="linklitagfooter"><a class="linkdivi" href="#">Email : <a class="emaillinks" href="mailto:info@voilastudio.in">info@voilastudio.in</a> | <a class="emaillinks" href="mailto:info@intenim.com">info@intenim.com</a></li>
+                </ul>
+            <hr class="footerlinkdivicenterofline">
+        </ul>
+
+
+            <ul class="footerlinkdivi5">
+                <li class="linklitagfooter"><a class="linkdivi highlight black" href="#">Branch Office</a></li>
+                <li class="linklitagfooter"><a style="font-weight: 700;" class="linkdivi" href="#">Intenim Technologies Pvt Ltd.</a></li>
+                <li class="linklitagfooter"><a class="linkdivi" href="#">1/1, First Floor, MS Ramaiah Rd, Gokula 1st
+                Stage, Mathikere Extension, Mathikere
+                Bengaluru, Karnataka 560054 India</a></li>
+                <li class="linklitagfooter"><a class="linkdivi" href="tel:+91 9711499548">Mobile : +91 9711499548</a></li>
+            </ul>
+        </div>
+
+        <ul class="footerlinkinmobilecenter">
+            <p style="text-align: center;" class="getintouchmesagemob highlight">Get In Touch !</p>
+            <ul class="footerlinkdivi3" style="margin-top: 35px;">
+                <li class="linklitagfooter numberslinks">Phone :
+                <a style="line-height: 24px;" class="linkdivi" href="tel:+919810554548"> +91 9810554548 |</a>
+                <a style="line-height: 24px;" class="linkdivi" href="tel:+919821554548">+91 9821554548 |</a>
+                <a style="line-height: 24px;" class="linkdivi" href="tel:+919821654548">+91 9821654548 </a>
+                <!-- <a style="line-height: 24px;" class="linkdivi" href="tel:+919560628008">+91 9560628008</a> -->
+        </li>
+                <li class="linklitagfooter"><a class="linkdivi" href="#">Email : <a class="emaillinks" href="mailto:info@voilastudio.in">info@voilastudio.in</a> | <a class="emaillinks" href="mailto:info@intenim.com">info@intenim.com</a></li>
+                </ul>
+        </ul>
+
+
+
+    </div>
+</section>
+
+<section class="mobileViewfooter">
+<div class="newfooterco">
+                <p class="topheadings pink">Quick Links</p>
+                <div class="underdivtext">
+                   <a class="paraheadings blackm" href="booking_calender.php">Model calender<span class="sepratorline">|</span></a> 
+                   <a class="paraheadings blackm" href="booking_calender_product.php">Product calender<span class="sepratorline">|</span></a> 
+                   <a class="paraheadings blackm" href="profile_current_booking.php">Manage Orders<span class="sepratorline">|</span></a>
+                   <a class="paraheadings blackm" href="voilagram.php" target="_blank">Social<span class="sepratorline">|</span></a> 
+                   <!-- <a class="paraheadings blackm" href="sitemap.xml">Studio live<span class="sepratorline">|</span></a> -->
+                   <a class="paraheadings blackm" href="">FAQ</a> 
+                   <!-- <a class="paraheadings black" href="">Home & Living</a>  -->
+                </div>
+            </div>
+
+            <div class="newfooterco">
+                <p class="topheadings pink">Info</p>
+                <div class="underdivtext">
+                   <a class="paraheadings blackm" href="aboutus.php">About us<span class="sepratorline">|</span></a> 
+                   <a class="paraheadings blackm" href="pricing.php">Pricing<span class="sepratorline">|</span></a> 
+                   <a class="paraheadings blackm" href="model_profile.php">Models<span class="sepratorline">|</span></a> 
+                   <a class="paraheadings blackm" href="contactus.php">Contact us<span class="sepratorline">|</span></a> 
+                   <a class="paraheadings blackm" href="career.php">Career<span class="sepratorline">|</span></a> 
+                   <a class="paraheadings blackm" href="sitemap.xml">Sitemap</a> 
+                </div>
+            </div>
+
+            <div class="newfooterco">
+                <p class="topheadings pink">Policies</p>
+                <div class="underdivtext">
+                   <a class="paraheadings blackm" href="/product-disptach-policy.php">Product Dispatch Policy<span class="sepratorline">|</span></a> 
+                   <a class="paraheadings blackm" href="/">Terms of use<span class="sepratorline">|</span></a> 
+                   <a class="paraheadings blackm" href="/cancelation.php">Cancellation<span class="sepratorline">|</span></a> 
+                   <a class="paraheadings blackm" href="/privacypolicy.php">Privacy policy<span class="sepratorline">|</span></a> 
+                   <a class="paraheadings blackm" href="/Grievance_policy.php">Grievance officer<span class="sepratorline">|</span></a> 
+                   <a class="paraheadings blackm" href="/refundpolicy.php">Refund policy</a> 
+                </div>
+            </div>
+
+            <div class="newfooterco">
+                <p class="topheadings pink">Our Verticals</p>
+                <div class="underdivtext">
+                   <a class="paraheadings blackm" href="">VOILA Studio<span class="sepratorline">|</span></a> 
+                   <a class="paraheadings blackm" href="">VOILA Biz<span class="sepratorline">|</span></a> 
+                   <a class="paraheadings blackm" href="">VOILA Kart<span class="sepratorline">|</span></a> 
+                   <a class="paraheadings blackm" href="">VOILA Retail</a> 
+                </div>
+            </div>
+            
+            <div class="detaileddiv">
+                <!-- <img class="logo_voilastudio" src="./mega_menu/img/voila_studio_logo.svg" alt=""> -->
+                <!-- <p class="paraheadings blackm">© 2017-<?php echo date("Y"); ?> All Rights Reserved Made & Designed By</p>
+                <p class="topheadings pink"> Intenim Technologies Pvt. Ltd.</p> -->
+                <!-- <div class="social_icons">
+                    <a href="https://www.facebook.com/VoilaStudio.in/"><img class="social_img" src="<?php echo BASE_URL; ?>/mega_menu/img/footer/facebook.webp" alt=""></a>
+                    <a href="https://www.instagram.com/VoilaStudioTM/"><img class="social_img" src="<?php echo BASE_URL; ?>/mega_menu/img/footer/instagram.webp" alt=""></a>
+                    <a href="https://in.linkedin.com/showcase/voilastudio"><img class="social_img" src="<?php echo BASE_URL; ?>/mega_menu/img/footer/linkedin.webp" alt=""></a>
+                    <a href="https://twitter.com/studiovoila?lang=en"><img class="social_img" src="<?php echo BASE_URL; ?>/mega_menu/img/footer/twitter.webp" alt=""></a>
+                    <a href="https://www.youtube.com/@voilastudio9023"><img class="social_img" src="<?php echo BASE_URL; ?>/mega_menu/img/footer/youtube.webp" alt=""></a>
+                </div> -->
+            </div>
+
+
+            <div class="addressdiv bottomfifteen">
+                <p class="topheadings bottomten pink">GET IN TOUCH !</p>
+                <p class="topheadings blackm borderbottom ">Corporate Office</p>
+                <p class="topheadings grey">Intenim Technologies Pvt. Ltd.</p>
+                <p class="paraheadings grey">Unit 1007-1008A, WellDone TechPark, Gurugram, Haryana- 122018, India</p>
+                <a class="paraheadings grey" href="tel:+91 124 4003737">Landline : +91 124 4003737</a>
+            </div>
+
+            <div class="addressdiv bottomfifteen">
+                <p class="topheadings black borderbottom ">Branch Office</p>
+                <p class="topheadings grey">Intenim Technologies Pvt. Ltd.</p>
+                <p class="paraheadings grey" href="">1/1, First Floor, MS Ramaiah Rd, Gokula 1st
+                    Stage, Mathikere Extension, Mathikere
+                    Bengaluru, Karnataka 560054 India</p>
+                <a class="paraheadings grey" href="tel:+91 9711499548">Mobile : +91 9711499548</a>
+            </div>
+            <div class="underdivtext">
+                <a class="paraheadings grey" href="tel:+91 9810554548">Phone : +91 9810554548 <span class="sepratorline">|</span></a>
+                <a class="paraheadings grey" href="tel:+91 9821554548">+91 9821554548 <span class="sepratorline">|</span></a>
+                <a class="paraheadings grey" href="tel:+91 9821654548">+91 9821654548 </a>
+                <!-- <a class="paraheadings grey" href="tel:+91 9560628008">+91 9560628008 </a> -->
+            </div>
+            <div class="underdivtext">
+            <a class="paraheadings blackm" href="mailto:info@voilastudio.in">Email : info@voilastudio.in <span class="sepratorline">|</span></a>
+            <a class="paraheadings blackm" href="mailto:info@intenim.com">info@intenim.com</a>
+                </div>
+</section>
+<hr style="width:100%; text-align:center">
+<div style="width:100%; align-item:center ;padding:2%; text-align:center">
+   <p class="linklitagfooter"><a class="linkdivi" href="#"> &#169; 2017-<?php echo date("Y"); ?> All Rights Reserved</a></p>
+                <p class="linklitagfooter"> Made & Designed By<br><a class="linkdivi highlight" href="#" style="font-size:12px"> Intenim Technologies Pvt Ltd.</a></p>
+</div>
+
+<script>
+
+
+/*
+
+        $.ajax({
+			type: "POST",
+            url: 'https://sub.voilastudio.co.in/view_cart_details_new.php',          
+            data: {
+                
+                myData:localStorage.getItem('myArray'),
+                myData_multiproduct:localStorage.getItem('multiproductArray'),
+                myData_product:localStorage.getItem('productArray'),
+                data_values:true
+            },
+			cache: false,
+            //dataType: "JSON",
+            success: function (data){
+         		data_view  = data.split("totalpay");
+			    $("#checkout_cart_details").html(data_view[0]);
+			    $("#total_pricing_without_gst").text('₹ '+data_view[1]);
+			    $("#gst_price").text('₹ '+data_view[2]);
+			    $("#total_pricing_with_gst").text('₹ '+data_view[3]);
+			    
+			   // alert(data_view[4]);
+			    
+                    if(data_view[4] > 0){
+                    $("#total_cart_items_modelsen").val(data_view[4]);
+                    $("#total_cart_items_modelsen").show();
+                    }else{
+                        $("#total_cart_items_modelsen").hide();
+                        
+                    }     
+
+			    $("#total_cart_items").val(data_view[4]);
+			    
+
+			    
+		   },
+			error: function(jqXHR, textStatus, errorThrown){
+				  alert('error');
+			  } 
+        });
+	
+*/
+
+
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
