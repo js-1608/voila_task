40fbf4963bfd8874bf9614c0244c2bf113f5e2c0
shivam <141029609+js-1608@users.noreply.github.com>
2024-08-03 14:12:46 +0530

    up

---
+++ outdoor_shoot.php
@@ -0,0 +1,552 @@
+
+<!-----------------
+voila old website
+------------------->
+<?php 
+include('old_website_assets/voilastudio_admin/lib/config.php');
+?>
+
+<!DOCTYPE html>
+<?php include("menu_test.php"); ?> 
+
+<html lang="zxx">
+
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>International Models For E-commerce Shoot | Amazon, Flipkart, Myntra Shoot | Voila Studio</title>
+	<!-- Meta tag Keywords -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<meta name="keywords" content="" />
+	<meta name="Description" content="Searching for International models for E-commerce shoots? Get E-commerce model shoot for Amazon, Flipkart, Myntra, Paytm, Limeroad, Nykaa & more. Contact us now! 
+" />
+	<script>
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!-- //Meta tag Keywords -->
+
+	<!-- Custom-Files -->
+	<link rel="stylesheet" href="./old_website_assets/css/bootstrap.css">
+	<!-- Bootstrap-Core-CSS -->
+	<link rel="stylesheet" href="./old_website_assets/css/lightbox.css" type="text/css" media="all">
+	<!-- Lightbox -->
+	<link rel="stylesheet" href="./old_website_assets/css/style.css" type="text/css" media="all" />
+	<!-- Style-CSS -->
+	<link rel="stylesheet" href="./old_website_assets/css/font-awesome.css">
+	<!-- Font-Awesome-Icons-CSS -->
+	<!-- //Custom-Files -->
+
+	<!-- Web-Fonts -->
+	<link href="//fonts.googleapis.com/css?family=Hind:300,400,500,600,700&amp;subset=devanagari,latin-ext" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=latin-ext"
+	 rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Great+Vibes&amp;subset=latin-ext" rel="stylesheet">
+	<link rel="icon" href="images/favicon.png" type="image/gif" sizes="25x25">
+	
+<!--	<script type="text/javascript">function add_chatinline(){var hccid=70599526;var nt=document.createElement("script");nt.async=true;nt.src="https://mylivechat.com/chatinline.aspx?hccid="+hccid;var ct=document.getElementsByTagName("script")[0];ct.parentNode.insertBefore(nt,ct);}-->
+<!--add_chatinline();-->
+<!--</script>-->
+	<!-- //Web-Fonts -->
+	
+	<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '2876709739029990'); 
+fbq('track', 'PageView');
+</script>
+<noscript>
+<img height="1" width="1" 
+src="https://www.facebook.com/tr?id=2876709739029990&ev=PageView
+&noscript=1"/>
+</noscript>
+<!-- End Facebook Pixel Code -->
+<style>
+    .mid
+    {
+        text-align:center;
+    }
+    .txt{
+        color:black;
+    }
+    .img-hover-zoom {
+      overflow: hidden;
+      margin: 0;
+}
+</style>
+
+<!--Start of Tawk.to Script
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5e687ed48d24fc226586edcf/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
+</head>
+
+<body>
+	<!-- banner -->
+
+	<!-- page details -->
+	<div class="breadcrumb-agile">
+		<nav aria-label="breadcrumb">
+			<ol class="breadcrumb">
+				<li class="breadcrumb-item">
+					<a href="index.php">Home</a>
+				</li>
+				<li class="breadcrumb-item active" aria-current="page">Models</li>
+			</ol>
+		</nav>
+	</div>
+	<!-- //page details -->
+
+	<!-- gallery -->
+	<div class="gallery-agile py-5">
+		<div class="container py-xl-5 py-lg-3">
+			<!-- heading title -->
+			<div class="text-center mb-lg-5 mb-4">
+				<h3 class="title-wthree mb-2">
+					New Trends<span class="mt-2 text-uppercase font-weight-bold">Outdoor Shoot</span></h3>
+			</div>
+			 
+			
+			<!-- //heading title -->
+			<div class="row gallery-agile-kmsrow">
+				<div class="col-md-4 col-sm-6 gallery-agile-grids">
+					<div class="portfolio-hover img-hover-zoom cont">
+						<a href="./old_website_assets/images/outdoor/_MG_8924.webp" data-lightbox="example-set" data-title="">
+							<img src="./old_website_assets/images/outdoor/_MG_8924.webp" class=" img-fluid zoom-img imgBox  " alt="" />
+						
+							
+							 
+						</a>
+					</div>
+				
+				</div>
+				
+	 				  
+			<div class="col-md-4 col-sm-6 gallery-agile-grids mt-sm-0 mt-4 ">
+					<div class="portfolio-hover img-hover-zoom cont">
+						 <a href="./old_website_assets/images/outdoor/_MG_8927.webp" data-lightbox="example-set" data-title="">
+							<img src="./old_website_assets/images/outdoor/_MG_8927.webp" class="img-fluid zoom-img imgBox" alt="" />
+							
+						</a>
+					</div>
+				</div>
+				
+				
+				<div class="col-md-4 col-sm-6 gallery-agile-grids mt-md-0 mt-4 ">
+					<div class="portfolio-hover img-hover-zoom cont">
+						 <a href="./old_website_assets/images/outdoor/_MG_8928.webp" data-lightbox="example-set" data-title="">
+							<img src="./old_website_assets/images/outdoor/_MG_8928.webp" class="img-fluid zoom-img imgBox" alt="" />
+							
+						</a>
+					</div>
+				</div>
+			<div class="col-md-4 col-sm-6 gallery-agile-grids my-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+					<a href="./old_website_assets/images/outdoor/_MG_8992.webp" data-lightbox="example-set" data-title="">
+							<img src="./old_website_assets/images/outdoor/_MG_8992.webp" class="img-fluid zoom-img imgBox" alt="" />
+							
+						</a>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-6 gallery-agile-grids my-md-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+						<a href="./old_website_assets/images/outdoor/_MG_9003.webp" data-lightbox="example-set" data-title="">
+							<img src="./old_website_assets/images/outdoor/_MG_9003.webp" class="img-fluid zoom-img imgBox" alt="" />
+								
+						</a>
+					</div>
+				</div>
+				
+				
+				
+				
+				
+				
+				<div class="col-md-4 col-sm-6 gallery-agile-grids my-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+							<a href="./old_website_assets/images/outdoor/_MG_9084.webp" data-lightbox="example-set" data-title="">
+							<img src="./old_website_assets/images/outdoor/_MG_9084.webp" class="img-fluid zoom-img imgBox" alt="" />
+								
+						</a>
+					</div>
+				</div>
+				
+					<div class="col-md-4 col-sm-6 gallery-agile-grids mt-md-0 mt-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+						<a href="./old_website_assets/images/outdoor/_MG_9127.webp" data-lightbox="example-set" data-title="">
+							<img src="./old_website_assets/images/outdoor/_MG_9127.webp" class="img-fluid " alt="" />
+							
+						</a>
+					</div>
+				</div>
+				
+					<div class="col-md-4 col-sm-6 gallery-agile-grids mt-md-0 mt-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+							<a href="./old_website_assets/images/outdoor/_MG_9172.webp" data-lightbox="example-set" data-title="">
+							<img src="./old_website_assets/images/outdoor/_MG_9172.webp" class="img-fluid " alt="" /> 
+							
+						</a>
+					</div>
+				</div>
+				
+				<div class="col-md-4 col-sm-6 gallery-agile-grids my-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+						<a href="./old_website_assets/images/outdoor/_MG_9191.webp" data-lightbox="example-set" data-title="">
+							<img src="./old_website_assets/images/outdoor/_MG_9191.webp" class="img-fluid zoom-img imgBox" alt="" />
+							
+						</a>
+					</div>
+				</div>
+				
+				<div class="col-md-4 col-sm-6 gallery-agile-grids my-md-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+						<a href="./old_website_assets/images/outdoor/_MG_9206.webp" data-lightbox="example-set" data-title="">
+							<img src="./old_website_assets/images/outdoor/_MG_9206.webp" class="img-fluid zoom-img imgBox" alt="" />
+								
+						</a>
+					</div>
+				</div>
+				
+				<!--	<div class="col-md-4 col-sm-6 gallery-agile-grids my-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+					<a href="images/lookbook/sku/3/9V0A5811.webp" data-lightbox="example-set" data-title="">
+							<img src="images/lookbook/sku/3/9V0A5811.webp" class="img-fluid zoom-img imgBox" alt="" />
+							
+						</a>
+					</div>
+				</div>
+				
+				<div class="col-md-12 mid">
+				    <hr>
+			      <h3><span class="txt" >Gown Shoot</span></h3>
+			      <hr>
+			          </div>
+				
+				
+					<div class="col-md-4 col-sm-6 gallery-agile-grids mt-md-0 mt-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+						<a href="images/lookbook/sku/4/9V0A0412.webp" data-lightbox="example-set" data-title="">
+							<img src="images/lookbook/sku/4/9V0A0412.webp" class="img-fluid zoom-img imgBox" alt="" />
+							
+						</a>
+					</div>
+				</div>
+				
+					<div class="col-md-4 col-sm-6 gallery-agile-grids mt-md-0 mt-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+						<a href="images/lookbook/sku/4/9V0A0415.webp" data-lightbox="example-set" data-title="">
+							<img src="images/lookbook/sku/4/9V0A0415.webp" class="img-fluid " alt="" />
+						
+						</a>
+					</div>
+				</div>
+				
+					<div class="col-md-4 col-sm-6 gallery-agile-grids mt-md-0 mt-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+					<a href="images/lookbook/sku/4/9V0A0423.webp" data-lightbox="example-set" data-title="">
+							<img src="images/lookbook/sku/4/9V0A0423.webp" class="img-fluid " alt="" /> 
+							
+						</a>
+					</div>
+				</div>
+				
+			
+				
+						<div class="col-md-4 col-sm-6 gallery-agile-grids my-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+						<a href="images/lookbook/sku/4/9V0A0419.webp" data-lightbox="example-set" data-title="">
+							<img src="images/lookbook/sku/4/9V0A0419.webp" class="img-fluid zoom-img imgBox" alt="" />
+						
+						</a>
+					</div>
+				</div>
+				
+				<div class="col-md-4 col-sm-6 gallery-agile-grids my-md-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+					<a href="images/lookbook/sku/4/9V0A0427.webp" data-lightbox="example-set" data-title="">
+							<img src="images/lookbook/sku/4/9V0A0427.webp" class="img-fluid " alt="" />
+						
+						</a>
+					</div>
+				</div>
+				
+					
+				
+				
+					<div class="col-md-4 col-sm-6 gallery-agile-grids my-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+						<a href="images/lookbook/sku/4/9V0A0434.webp" data-lightbox="example-set" data-title="">
+							<img src="images/lookbook/sku/4/9V0A0434.webp" class="img-fluid " alt="" /> 
+						
+						</a>
+					</div>
+				</div>
+				
+				<div class="col-md-12 mid">
+				    <hr>
+			      <h3><span class="txt" >Kurta & Plazzo Shoot</span></h3>
+			      <hr>
+			          </div>
+				
+				
+					<div class="col-md-4 col-sm-6 gallery-agile-grids mt-md-0 mt-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+						<a href="images/lookbook/sku/6/9V0A0261.webp" data-lightbox="example-set" data-title="">
+							<img src="images/lookbook/sku/6/9V0A0261.webp"  class="img-fluid zoom-img imgBox" alt="" />
+							
+						</a>
+					</div>
+				</div>
+				
+					<div class="col-md-4 col-sm-6 gallery-agile-grids mt-md-0 mt-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+							<a href="images/lookbook/sku/6/9V0A0265.webp" data-lightbox="example-set" data-title="">
+							<img src="images/lookbook/sku/6/9V0A0265.webp" class="img-fluid " alt="" />
+						
+						</a>
+					</div>
+				</div>
+				
+					<div class="col-md-4 col-sm-6 gallery-agile-grids mt-md-0 mt-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+						<a href="images/lookbook/sku/6/9V0A0267.webp" data-lightbox="example-set" data-title="">
+							<img src="images/lookbook/sku/6/9V0A0267.webp" class="img-fluid " alt="" /> 
+							
+						</a>
+					</div>
+				</div>
+				
+							<div class="col-md-4 col-sm-6 gallery-agile-grids my-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+							<a href="images/lookbook/sku/6/9V0A0259.webp" data-lightbox="example-set" data-title="">
+							<img src="images/lookbook/sku/6/9V0A0259.webp" class="img-fluid zoom-img imgBox" alt="" />
+						
+						</a>
+					</div>
+				</div>
+				
+				<div class="col-md-4 col-sm-6 gallery-agile-grids my-md-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+						<a href="images/lookbook/sku/6/9V0A0268.webp" data-lightbox="example-set" data-title="">
+							<img src="images/lookbook/sku/6/9V0A0268.webp" class="img-fluid " alt="" />
+							
+						</a>
+					</div>
+				</div>
+				
+				
+				
+					<div class="col-md-12 mid">
+				    <hr>
+			      <h3><span class="txt" >Waistcoat Shoot</span></h3>
+			      <hr>
+			          </div>
+				
+					<div class="col-md-4 col-sm-6 gallery-agile-grids mt-md-0 mt-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+					<a href="images/lookbook/sku/men/9V0A6229.webp" data-lightbox="example-set" data-title="">
+							<img src="images/lookbook/sku/men/9V0A6229.webp" class="img-fluid zoom-img imgBox" alt="" />
+							
+						</a>
+					</div>
+				</div>
+				
+					<div class="col-md-4 col-sm-6 gallery-agile-grids mt-md-0 mt-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+					<a href="images/lookbook/sku/men/9V0A6234.webp" data-lightbox="example-set" data-title="">
+							<img src="images/lookbook/sku/men/9V0A6234.webp" class="img-fluid " alt="" />
+							
+						</a>
+					</div>
+				</div>
+				
+					<div class="col-md-4 col-sm-6 gallery-agile-grids mt-md-0 mt-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+						<a href="images/lookbook/sku/men/9V0A6237.webp" data-lightbox="example-set" data-title="">
+							<img src="images/lookbook/sku/men/9V0A6237.webp" class="img-fluid " alt="" /> 
+						
+						</a>
+					</div>
+				</div>
+				
+						<div class="col-md-4 col-sm-6 gallery-agile-grids my-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+						<a href="images/lookbook/sku/men/9V0A6239.webp" data-lightbox="example-set" data-title="">
+							<img src="images/lookbook/sku/men/9V0A6239.webp" class="img-fluid zoom-img imgBox" alt="" />
+							
+						</a>
+					</div>
+				</div>
+				
+				<div class="col-md-4 col-sm-6 gallery-agile-grids my-md-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+						<a href="images/lookbook/sku/men/9V0A6240.webp" data-lightbox="example-set" data-title="">
+							<img src="images/lookbook/sku/men/9V0A6240.webp" class="img-fluid " alt="" />
+							
+						</a>
+					</div>
+				</div>
+				
+					<div class="col-md-12 mid">
+				    <hr>
+			      <h3><span class="txt" >Lehenga Shoot</span></h3>
+			      <hr>
+			          </div>
+				
+					<div class="col-md-4 col-sm-6 gallery-agile-grids mt-md-0 mt-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+						<a href="images/lookbook/sku/10/_V0A2022.webp" data-lightbox="example-set" data-title="">
+							<img src="images/lookbook/sku/10/_V0A2022.webp" class="img-fluid zoom-img imgBox" alt="" />
+						
+						</a>
+					</div>
+				</div>
+				
+					<div class="col-md-4 col-sm-6 gallery-agile-grids mt-md-0 mt-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+						<a href="images/lookbook/sku/10/_V0A2023.webp" data-lightbox="example-set" data-title="">
+							<img src="images/lookbook/sku/10/_V0A2023.webp" class="img-fluid " alt="" />
+							
+						</a>
+					</div>
+				</div>
+				
+					<div class="col-md-4 col-sm-6 gallery-agile-grids mt-md-0 mt-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+							<a href="images/lookbook/sku/10/_V0A2025.webp" data-lightbox="example-set" data-title="">
+							<img src="images/lookbook/sku/10/_V0A2025.webp" class="img-fluid " alt="" /> 
+						
+						</a>
+					</div>
+				</div>
+				
+						<div class="col-md-4 col-sm-6 gallery-agile-grids my-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+							<a href="images/lookbook/sku/10/_V0A2039.webp" data-lightbox="example-set" data-title="">
+							<img src="images/lookbook/sku/10/_V0A2039.webp" class="img-fluid zoom-img imgBox" alt="" />
+						
+						</a>
+					</div>
+				</div>
+				
+				<div class="col-md-4 col-sm-6 gallery-agile-grids my-md-4">
+					<div class="portfolio-hover img-hover-zoom cont">
+							<a href="images/lookbook/sku/10/_V0A2030.webp" data-lightbox="example-set" data-title="">
+							<img src="images/lookbook/sku/10/_V0A2030.webp" class="img-fluid " alt="" />
+							
+						</a>
+					</div>
+				</div>
+				-->
+			</div>
+		</div>
+	</div>
+	<!-- //gallery -->
+	
+	<!-- //Botom Line -->
+	
+	<div class="container-fluid">
+	     <div class="row">
+	         <div class="col-sm-12 text-center ">
+	             <hr>
+	             <ul>	
+	                 <li class=" ">
+	                     <P>
+	                         THESE IMAGES ARE REFERENCE PURPOSE ONLY 
+	                     </P>
+	                     
+	                 </li>
+	                 
+	                
+	             </ul>
+	             
+	         </div>
+	         
+	     </div>
+
+<!-- Botom content button -->       
+    <?php include_once("content_button.php"); ?>
+   <!-- //Botom content button -->
+	      
+   <!-- footer -->
+       <?php include_once("footer.php"); ?>
+
+   <!-- //footer -->
+
+
+	<!-- Js files -->
+	<!-- JavaScript -->
+	<script src="./old_website_assets/js/jquery-2.2.3.min.js"></script>
+	<!-- Default-JavaScript-File -->
+
+	<!-- navigation -->
+	<!-- dropdown smooth -->
+	<script>
+		$(document).ready(function () {
+			$(".dropdown").hover(
+				function () {
+					$('.dropdown-menu', this).stop(true, true).slideDown("fast");
+					$(this).toggleClass('open');
+				},
+				function () {
+					$('.dropdown-menu', this).stop(true, true).slideUp("fast");
+					$(this).toggleClass('open');
+				}
+			);
+		});
+	</script>
+	<!-- //dropdown smooth -->
+	<!-- fixed nav -->
+	<script>
+		$(window).scroll(function () {
+			if ($(document).scrollTop() > 50) {
+				$('nav').addClass('shrink');
+			} else {
+				$('nav').removeClass('shrink');
+			}
+		});
+	</script>
+	<!-- //fixed nav -->
+	<!-- //navigation -->
+
+	<!--  light box js -->
+	<script src="./old_website_assets/js/lightbox-plus-jquery.min.js"></script>
+	<!-- //light box js-->
+
+	<!-- smooth scrolling -->
+	<script src="./old_website_assets/js/SmoothScroll.min.js"></script>
+	<!-- move-top -->
+	<script src="./old_website_assets/js/move-top.js"></script>
+	<!-- easing -->
+	<script src="./old_website_assets/js/easing.js"></script>
+	<!--  necessary snippets for few javascript files -->
+	<script src="./old_website_assets/js/look.js"></script>
+
+	<script src="./old_website_assets/js/bootstrap.js"></script>
+	<!-- Necessary-JavaScript-File-For-Bootstrap -->
+	<!-- //Js files -->
+
+
+</body>
+
